3da015d40e6094f2b9af90e70a6d232ecc41693f
DaisyChepyegon <daisychepyegon@gmail.com>
2022-06-09 11:03:38 +0300

    Done.

--- index.js
+++ index.js
@@ -4,13 +4,9 @@
 
     function superbowlWin(record){
         const result = record.find( record => record.result === "W" );
-        return !!result ? result.year : undefined;
+        return result ? result.year : undefined;
     }
 
         
     
   
-/*superbowlWin = (record) => {
-    const result = record.find( record => record.result === "W" );
-    return !!result ? result.year : undefined;
-  }*/
